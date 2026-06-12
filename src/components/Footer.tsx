b920102bc0b03e42aba333b60d6bbbd9fe2e50bf
Arturo70876 <fgutierrezmontano824@gmail.com>
2026-06-12 16:55:18 -0600

    Gojo - Continue design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,23 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
+
       <div className="footer__content">
-        <div className="navbar__brand">
-        <a href="/">
-          <span className="navbar__logo">G</span>
-          <span>Galliard UI</span>
-        </a>
-      </div>
+        
+        <div className="footer__brand">
+          <a href="/">
+            <span className="footer__logo">G</span>
+            <span className="footer__title">Galliard UI</span>
+          </a>
+        </div>
+
         <p>Contáctanos: contacto@miweb.com</p>
 
         <ul className="footer__links">
           <li>
             <a href="/sobre-nosotros">Sobre nosotros</a>
           </li>
+
           <li>
             <a href="/aviso-legal">Aviso legal</a>
           </li>
