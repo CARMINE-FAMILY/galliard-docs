697619fd8144dea0ff1931f6ec3686384c47f2b9
Arturo70876 <fgutierrezmontano824@gmail.com>
2026-06-14 21:51:20 -0600

    Megumi - Add glass morphism

--- src/app/landingPage/LandigScreen.tsx
+++ src/app/landingPage/LandigScreen.tsx
@@ -1,9 +1,15 @@
 import Hero from "../../components/Hero";
+import Final from "../Final";
+import Nose from "../Nose";
+import Prueba from "../Prueba";
 
 export default function LandingScreen() {
   return (
     <div className="landing-screen">
       <Hero />
+      <Prueba />
+      <Nose />
+      <Final />
     </div>
   );
 }
